9f9a2f65923a2868b380d41e76e9a6166d615c0c
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-08-25 13:28:22 +0200

    Form component to add a new task to the to-do list

---
+++ src/pages/index.tsx
@@ -0,0 +1,34 @@
+
+import { useState } from 'react';
+// Input field to capture the title of the new task.
+const Home = () => {
+  const [task, setTask] = useState<string>('');
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>My To-Do List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add Task</button>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
